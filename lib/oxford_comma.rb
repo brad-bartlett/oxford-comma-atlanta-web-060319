45b2eb38b8f719e1efccfd36f69039f939207b83
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-24 05:41:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-array = ["apple"
+array = ["apple"]
 
 def oxford_comma(array)
   if array.length == 2
@@ -11,5 +11,5 @@ def oxford_comma(array)
   elsif array.length > 2
     array[-1].prepend "and "
     array.join(", ")
-  end  
+  end
 end
